3cb382279643657c6104d535ae4da65f4e2257b3
Ap.Muthu <apmuthu@usa.net>
2014-02-12 16:40:52 +0530

    Code in core folder, updated to POSNIC 1.02 Design Fix.zip http://sourceforge.net/projects/stockmanagement/files/POSNIC%201.02%20Design%20Fix.zip

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_destroy();
-unset($_SESSION["id"]);
-unset($_SESSION["username"]);
-unset($_SESSION["usertype"]);
-header("location:index.php?msg=You%20have%20been%20logged%20out!&type=information");
-?>
